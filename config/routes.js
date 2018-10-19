66fdf75f1af6dba20d1134e101dd80852f6c9d64
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-19 13:18:18 -0400

    Register & Login funtions working**

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
+const jwtKey = require('../_secrets/keys').jwtKey;
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('./middlewares');
 
@@ -8,12 +12,54 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn:'1h',
+    jwtid:'12345',
+  }
+  return jwt.sign(payload, jwtKey, options)
+}
+
 function register(req, res) {
   // implement user registration
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 12);
+  credentials.password = hash;
+  db('users')
+  .insert(credentials)
+  .then(ids => {
+    const id = ids[0];
+
+    db('users')
+    .where({ id })
+    .first()
+    .then(user => {
+      const token = generateToken(user);
+      res.status(201).json({ id: user.id, token});
+    })
+    .catch(err => res.status(500).send(err));
+  })
+  .catch(err => res.status(500).send(err));
 }
 
 function login(req, res) {
   // implement user login
+  const credentials = req.body;
+  db('users')
+  .where('username',credentials.username)
+  .first()
+  .then(user => {
+   if(user && bcrypt.compareSync(credentials.password, user.password)) {
+     const token = generateToken(user);
+     res.status(200).json({ token });
+   } else {
+     res.status(401).json({ message: 'invalid credentials'});
+   }
+  })
+  .catch(err => res.status(500).send(err));
 }
 
 function getJokes(req, res) {
@@ -25,6 +71,6 @@ function getJokes(req, res) {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: 'Error getting those weak Jokes', error: err });
     });
 }
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { server } = require('./server.js');
 
-const port = process.env.PORT || 3300;
+server.get('/', (req, res) => {
+  res.send('This thing running...')
+});
+
+const port = process.env.PORT || 9001;
 server.listen(port, () => {
   console.log(`\n=== Server listening on port ${port}\n`);
 });
